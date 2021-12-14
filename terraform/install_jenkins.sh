7994a2d5012475dfc15e286c2f9c32e993a30d5e
unknown <U683225@bxo.local>
2021-12-14 09:49:36 +0900

    fix sh

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -15,15 +15,17 @@ yum install -y git
 
 echo "Install Docker engine"
 yum update -y
-yum install docker -y
+sudo amazon-linux-extras install docker
 #sudo usermod -a -G docker jenkins
 #sudo service docker start
 sudo chkconfig docker on
 
 echo "Install Jenkins"
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-yum install -y jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo amazon-linux-extras install epel -y
+sudo yum update -y
+sudo yum install jenkins java-1.8.0-openjdk-devel
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
